f06d7e23fc8627dbbc4fac6efae07d4f2ceb23fa
Zeke Fast <zekefast@gmail.com>
2023-09-13 15:39:34 +0200

    Bootstrap axum. Listen on 127.0.0.1:8080 address.
    Changes:
    - Bootstrap axum web framework listening on 127.0.0.1:8080 address.
    - Declare "/api" path for API routes.
    - Inroduce crate::api module.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,29 @@
 mod error;
 mod prelude;
 mod utils;
+mod api;
 
+use std::net::SocketAddr;
+use axum::handler::HandlerWithoutStateExt;
+use axum::Router;
 use crate::prelude::*;
+
+const DEFAULT_LISTEN_PORT: u16 = 8080;
+const DEFAULT_LISTEN_HOST: [u8; 4] = [127, 0, 0, 1];
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Hello, world!");
+    let listen_address = SocketAddr::from((DEFAULT_LISTEN_HOST, DEFAULT_LISTEN_PORT));
+
+    let routes_api = Router::new();
+
+    let routes_all = Router::new()
+        .nest("/api", routes_api);
+
+    axum::Server::bind(&listen_address)
+        .serve(routes_all.into_make_service())
+        .await
+        .unwrap();
 
     Ok(())
 }
