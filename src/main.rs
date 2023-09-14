be2c5fed4e87a55a41917f1a9de51d462bd54e53
Zeke Fast <zekefast@gmail.com>
2023-09-14 22:41:02 +0200

    Introduce pg_pool() function to create database connection pool.
    Changes:
    - Introduce pg_pool() function to create database connection pool using
      credentials provided in "DATABASE_URL" environment variables.
    - Create database connection pool in main() by calling pg_pool().

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,21 @@ mod prelude;
 mod utils;
 mod api;
 
-use std::net::SocketAddr;
+use std::{
+    net::SocketAddr,
+    time::Duration,
+};
 use axum::handler::HandlerWithoutStateExt;
 use axum::Router;
 
 use dotenvy::dotenv;
-
+use sqlx::{
+    postgres::PgPoolOptions,
+    PgPool
+};
 
 use crate::prelude::*;
+
 
 const DEFAULT_LISTEN_PORT: u16 = 8080;
 const DEFAULT_LISTEN_HOST: [u8; 4] = [127, 0, 0, 1];
@@ -20,6 +27,8 @@ const DEFAULT_LISTEN_HOST: [u8; 4] = [127, 0, 0, 1];
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().expect("failed to load .env");
+
+    let pool = pg_pool().await.expect("failed to connect to postgres");
 
     let listen_address = SocketAddr::from((DEFAULT_LISTEN_HOST, DEFAULT_LISTEN_PORT));
 
@@ -34,4 +43,14 @@ async fn main() -> Result<()> {
         .unwrap();
 
     Ok(())
+}
+
+pub async fn pg_pool() -> core::result::Result<PgPool, sqlx::Error> {
+    dotenv().expect("failed to load .env");
+
+    PgPoolOptions::new()
+        .max_connections(5)
+        .acquire_timeout(Duration::from_secs(1))
+        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be in environment"))
+        .await
 }
