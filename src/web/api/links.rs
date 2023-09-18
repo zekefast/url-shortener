a9c656858b38493a2e19b355c4ba2b0c81eed51a
Zeke Fast <zekefast@gmail.com>
2023-09-18 12:52:48 +0200

    Call link service's shorten() function from crate::web::api::create_shortened_link handler.o
    Additional changes:
    - Add "url" property to JSON response body with shortened url as value.

--- src/web/api/links.rs
+++ src/web/api/links.rs
@@ -3,6 +3,7 @@ use axum::Json;
 use serde::Deserialize;
 use serde_json::{json, Value};
 use url::Url;
+use crate::service::link;
 use crate::web::api::prelude::*;
 
 pub(super) async fn create_shortened_link(
@@ -11,7 +12,10 @@ pub(super) async fn create_shortened_link(
 ) -> Result<Json<Value>> {
     let url = Url::parse(&payload.url).map_err(Error::from)?;
 
+    let shortened_url = link::shorten(url, db);
+
     let body = Json(json!({
+        "url": shortened_url
     }));
 
     Ok(body)
